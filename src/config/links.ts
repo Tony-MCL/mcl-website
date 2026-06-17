54f61c76b7cc8e6e34d3919b78f9a6554ae9336d
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-06-17 18:05:26 +0200

    Update links.ts

--- src/config/links.ts
+++ src/config/links.ts
@@ -3,5 +3,5 @@ export const LINKS = {
   ms: "https://managesystem.no",
   progress: "https://progress.managesystem.no",
   kvittekGooglePlay: "https://play.google.com/store/apps/details?id=com.morningcoffeelabs.kvittek",
-  kvittekAppStore: "",
+  kvittekAppStore: "https://apps.apple.com/no/app/kvittek/id6777185540",
 } as const;
