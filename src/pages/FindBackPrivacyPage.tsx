84f423d722d1a0c09cb55bae9041eab39277aa03
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-07-30 18:22:02 +0200

    Add FindBack privacy page

---
+++ src/pages/FindBackPrivacyPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import FindBackLegalPage from "./FindBackLegalPage";
+
+const FindBackPrivacyPage: React.FC = () => {
+  return <FindBackLegalPage type="privacy" />;
+};
+
+export default FindBackPrivacyPage;
