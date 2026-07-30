4de32f5d0d79253573c74da8448b978bb844be6b
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-07-30 18:22:08 +0200

    Add FindBack terms page

---
+++ src/pages/FindBackTermsPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import FindBackLegalPage from "./FindBackLegalPage";
+
+const FindBackTermsPage: React.FC = () => {
+  return <FindBackLegalPage type="terms" />;
+};
+
+export default FindBackTermsPage;
