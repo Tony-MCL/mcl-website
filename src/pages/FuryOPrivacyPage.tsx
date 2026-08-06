26c46bfe01a3ede015177b223662c121a8342a08
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-06 14:17:04 +0200

    Add Fury O privacy page

---
+++ src/pages/FuryOPrivacyPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import FuryOLegalPage from "./FuryOLegalPage";
+
+const FuryOPrivacyPage: React.FC = () => {
+  return <FuryOLegalPage type="privacy" />;
+};
+
+export default FuryOPrivacyPage;
