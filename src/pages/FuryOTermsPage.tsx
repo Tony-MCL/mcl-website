d21c9c1a67011a04f57e5a0c4fbb27ca3a698c33
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-06 14:17:09 +0200

    Add Fury O terms page

---
+++ src/pages/FuryOTermsPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import FuryOLegalPage from "./FuryOLegalPage";
+
+const FuryOTermsPage: React.FC = () => {
+  return <FuryOLegalPage type="terms" />;
+};
+
+export default FuryOTermsPage;
