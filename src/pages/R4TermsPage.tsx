a30c08750004f83a358f6119daa46d71a1e755ed
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-14 07:30:38 +0200

    Add R4 terms page wrapper

---
+++ src/pages/R4TermsPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import R4LegalPage from "./R4LegalPage";
+
+const R4TermsPage: React.FC = () => {
+  return <R4LegalPage type="terms" />;
+};
+
+export default R4TermsPage;
