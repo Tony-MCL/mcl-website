660ef07dec17cd002a0cefe5e75920a57a57abc8
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-14 07:30:30 +0200

    Add R4 privacy page wrapper

---
+++ src/pages/R4PrivacyPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import R4LegalPage from "./R4LegalPage";
+
+const R4PrivacyPage: React.FC = () => {
+  return <R4LegalPage type="privacy" />;
+};
+
+export default R4PrivacyPage;
