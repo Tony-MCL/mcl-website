dcf39db06920d0e9746d6907cc0c8bf9a19a2fb4
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-08-29 09:00:34 +0200

    Add Mosaic ME privacy page

---
+++ src/pages/MosaicMePrivacyPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MosaicMeLegalPage from "./MosaicMeLegalPage";
+
+const MosaicMePrivacyPage: React.FC = () => {
+  return <MosaicMeLegalPage type="privacy" />;
+};
+
+export default MosaicMePrivacyPage;
