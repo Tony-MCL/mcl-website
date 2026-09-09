c4b6ae04c8a2e45539480f68e01ac59be0d9a2f0
Tony-MCL <MorningCoffeeLabs@gmail.com>
2026-09-09 12:18:05 +0200

    Fix FindBack PeerPush link

--- src/config/links.ts
+++ src/config/links.ts
@@ -8,7 +8,7 @@ export const LINKS = {
   husketGooglePlay: "https://play.google.com/store/apps/details?id=no.morningcoffeelabs.husket&pcampaignid=web_share",
   findbackGooglePlay: "https://play.google.com/store/apps/details?id=com.morningcoffeelabs.findback",
   findbackAppStore: "https://apps.apple.com/us/app/findback-onetap/id6796313821",
-  findbackPeerPush: "https://peerpush.com/p/findback",
+  findbackPeerPush: "https://peerpush.com/p/findback-cupi",
   furyGooglePlay: "https://play.google.com/store/apps/details?id=com.morningcoffeelabs.furyo",
   furyAppStore: "https://apps.apple.com/us/app/fury-o/id6798349790",
   furyPeerPush: "https://peerpush.com/p/fury-o",
